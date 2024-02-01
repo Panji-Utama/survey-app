9f80333d9c447301ba55d5fb21a13a8114c1c3ea
FIRMAN PANJI <fpu2468@gmail.com>
2024-02-01 22:58:34 +0700

    progress: JWT Implementation #1

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,111 +1,118 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const router = express.Router()
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL)
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
 
-const db = mongoose.connection
-const userCollection = db.collection('user');
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const db = mongoose.connection;
+const userCollection = db.collection("user");
 
-// Login
-// router.get('/auth', async (req, res) => {
-//   let email = req.body.email;
-//   let password = req.body.password;
-//   console.log(email, password)
+//* ============= Maker Register ================ *//
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ email, password: hashedPassword });
+    await user.save();
+    return res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: "Registration failed" });
+  }
+});
+// ============= Maker Register ================ //
 
-//   if (email && password) {
-//         try {
-//           const user = await userCollection.findOne({ "email": email, "password": password });
+//* ============= Maker Login ================ *//
+router.post("/maker/auth", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-//           if (user) {
-//               res.json(user);
-//           } else {
-//               res.status(404).json({ error: 'User not found' });
-//           }
-//       } catch (error) {
-//           res.status(500).json({ error: 'Internal server error' });
-//       }
-//   } else {
-//       res.status(400).send("Error: Please fill in email and password");
-//   }
-// });
+    if (!email) {
+      return res.status(400).json({ error: "Please fill in the email!" });
+    } else if (!password) {
+      return res.status(400).json({ error: "Please fill in your password!" });
+    } else {
+      const user = await userCollection.findOne({
+        email: email,
+        password: password,
+      });
 
-router.post("/maker/auth", async (req,res) => {
-    try {
-        const {email, password} = req.body
+      if (!user) {
+        res.status(404).json({ error: "User not found!" });
+      }
 
-        if (!email) {
-            return res.status(400).json({error: "Please fill in the email!"})
-        } else if (!password){
-            return res.status(400).json({error: "Please fill in your password!"})
-        } else {
-            const user = await userCollection.findOne({ "email": email, "password": password });
+      const passwordMatch = await bcrypt.compare(password, user.password);
 
-            if (!user) {
-                res.status(404).json({error: "User not found!"})
-            } else {
-                const {_id, username, email} = user
-                res.status(200).json({_id, username, email})
-            }
-        }
-    } catch (error) {
-        res.status(500).json({error: 'Internal server error'})
+      if (!passwordMatch) {
+        return res.status(401).json({ error: "Invalid password" });
+      }
+
+      const token = jwt.sign({ userId: user._id }, "secret-key", {
+        expiresIn: "1h",
+      });
+
+      res.status(200).json({ token });
     }
-})
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+// ============= Maker Login ================ //
 
-router.post("/filler/auth", async (req,res) => {
-    try {
-        const {email, password} = req.body
+//* ============= Filler Login ================ *//
+router.post("/filler/auth", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-        if (!email) {
-            return res.status(400).json({error: "Please fill in the email!"})
-        } else if (!password){
-            return res.status(400).json({error: "Please fill in your password!"})
-        } else {
-            const user = await userCollection.findOne({ "email": email, "password": password });
+    if (!email) {
+      return res.status(400).json({ error: "Please fill in the email!" });
+    } else if (!password) {
+      return res.status(400).json({ error: "Please fill in your password!" });
+    } else {
+      const user = await userCollection.findOne({
+        email: email,
+        password: password,
+      });
 
-            if (!user) {
-                res.status(404).json({error: "User not found!"})
-            } else {
-                const {_id, username, email} = user
-                res.status(200).json({_id, username, email})
-            }
-        }
-    } catch (error) {
-        res.status(500).json({error: 'Internal server error'})
+      if (!user) {
+        res.status(404).json({ error: "User not found!" });
+      } else {
+        const { _id, username, email } = user;
+        // const passwordMatch = await bcrypt.compare(password, user.)
+        res.status(200).json({ _id, username, email });
+      }
     }
-})
-
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+// ============= Filler Login ================ //
 
 // Get All
-router.get('/', async (req, res) => {
-    try {
-        // res.send("Hello World")
-        const user = await userCollection.find()
-        res.json(user)
-    } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  });
+router.get("/", async (req, res) => {
+  try {
+    // res.send("Hello World")
+    const user = await userCollection.find();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 // Get One
-router.get('/:id', (req,res) => {
-    res.send(req.params.id)
-})
+router.get("/:id", (req, res) => {
+  res.send(req.params.id);
+});
 
 // Create One
-router.post('/', (req,res) => {
-
-})
+router.post("/", (req, res) => {});
 
 // Update One
-router.patch('/:id', (req,res) => {
-
-})
+router.patch("/:id", (req, res) => {});
 
 // Delete One
-router.delete('/:id', (req,res) => {
-
-})
-module.exports = router
+router.delete("/:id", (req, res) => {});
+module.exports = router;
