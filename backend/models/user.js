ce66ccd9f9d8b6f61ac1a4b3ad9ced01ac19a5e8
FIRMAN PANJI <fpu2468@gmail.com>
2024-02-01 22:57:39 +0700

    feat: User Model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
-const usersSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    
-})
+const userSchema = new mongoose.Schema({
+    email: {type: String, required: true},
+    password: {type: String, required: true}
+});
+
+module.exports = mongoose.model('User', userSchema, "user")
