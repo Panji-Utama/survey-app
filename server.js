f8685dc92362885e4d409093119d2c94c15101ac
FIRMAN PANJI <fpu2468@gmail.com>
2024-02-20 22:20:59 +0700

    chore: Update Server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const path = require('path')
 const cors = require('cors')
 
 mongoose.connect(process.env.DATABASE_URL)
@@ -11,6 +10,8 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
+
+//* ============= CORS Config ================ *//
 const corsOpts = {
   origin: '*',
 
@@ -23,14 +24,23 @@ const corsOpts = {
     'Content-Type',
   ],
 };
-
 app.use(cors(corsOpts));
+// ============= CORS Config ================//
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 const usersRouter = require('./backend/routes/user')
+const protectRouter = require('./backend/routes/protectedRoute')
 app.use('/user', usersRouter)
+app.use('/protected', protectRouter)
+
+const errorHandler = require('./backend/middleware/errorMiddleware')
+app.use(errorHandler)
+
+const logger = require('./backend/middleware/loggerMiddleware')
+app.use(logger)
+
 
 const PORT = process.env.PORT || 3000
 
